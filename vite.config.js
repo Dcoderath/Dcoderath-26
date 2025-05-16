cbb58d7ca44da9195022bf38edd0224a65b70286
dcoderath <trivedi2693@gmail.com>
2025-05-17 00:16:47 +0530

    Update whole project with latest changes

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,7 @@
-// import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react';
-
-// export default defineConfig({
-//   base: '/Dcoderath-26/',  // This must match your repo name exactly (case sensitive!)
-//   plugins: [react()],
-// });
-
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/Dcoderath-26/',  // Only needed for GitHub Pages; safe to keep
+  base: '/Dcoderath-26/',  // This must match your repo name exactly (case sensitive!)
   plugins: [react()],
-  server: {
-    host: true,  // Exposes the dev server to your local network
-    port: 5173,  // Or any open port
-  },
 });
